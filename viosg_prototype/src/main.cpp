e16301370c577e763eea24d2abb4041d2242bb1b
lamboris90 <lamboris90@gmail.com>
2013-11-05 09:25:22 +0100

    Analyze the structure of the scene

--- viosg_prototype/src/main.cpp
+++ viosg_prototype/src/main.cpp
@@ -6,8 +6,13 @@
 #include <osgViewer/ViewerEventHandlers>
 #include <osgGA/StateSetManipulator>
 
+#include <osg/Group>
 #include <osgDB/ReadFile>
+#include <osgDB/Registry>
+#include <iostream>
+USE_OSGPLUGIN(citygml)
 
+using namespace std;
 int main()
 {
 /* OBJECTS CREATION */
@@ -15,13 +20,46 @@ int main()
 
         osgViewer::Viewer viewer ;
         osg::ref_ptr<osg::Group> root (new osg::Group);
-        //Creating the model node
-        osg::ref_ptr<osg::Node> fileNode (osgDB::readNodeFile("/home/blam/samples/Frankfurt_Street_Setting_LOD3/Frankfurt_Street_Setting_LOD3.citygml"));
+/* TESTS */
 
-        // Set the scene data
-        root->addChild(fileNode.get());
-        viewer.setSceneData( root.get() );
+        //Chargement du fichier
+        //osg::ref_ptr<osg::Node> fileNode (osgDB::readNodeFile("/home/blam/samples/1.citygml"));
+          osg::ref_ptr<osg::Node> fileNode (osgDB::readNodeFile("/home/blam/samples/Frankfurt_Street_Setting_LOD3/Frankfurt_Street_Setting_LOD3.citygml"));
+        //Objets pour tester le fichier
+        osg::ref_ptr <osg::Group> fileGroup;
+        osg::ref_ptr <osg::Group> subGroup1;
+        osg::ref_ptr <osg::Group> subGroup2;
+        osg::ref_ptr <osg::Group> subGroup3;
 
+
+        //Donne comme résultat MatrixTransform, on peut donc le caster en group
+        cout << fileNode->className() << endl;
+        // Caste le node en group pour pouvoir le décomposer
+        fileGroup = (osg::Group*) fileNode.get();
+        // Parcoure les enfants de la scène à 3 niveaux par des boucles
+        for (int i = 0; i < fileGroup->getNumChildren(); i++) {
+           cout << "\t" << fileGroup->getChild(i)->className() << endl;
+
+           subGroup1 = (osg::Group *) fileGroup->getChild(i);
+           for (int j = 0; j < subGroup1->getNumChildren(); j++) {
+              cout << "\t\t" << subGroup1->getChild(j)->className() << endl;
+
+              subGroup2 = (osg::Group *) subGroup1->getChild(j);
+              for (int k = 0; k < subGroup2->getNumChildren(); k++) {
+                 cout << "\t\t\t" << subGroup2->getChild(k)->className() << endl;
+
+                 subGroup3 = (osg::Group *) subGroup2->getChild(k);
+                 for (int l = 0; l < subGroup3->getNumChildren(); l++) {
+                    cout << "\t\t\t\t" << subGroup3->getChild(l)->className() << endl;
+                 }
+              }
+           }
+        }
+
+        /* SCENE DATA */
+
+         root->addChild(fileNode.get());
+         viewer.setSceneData( root.get() );
 /* KEYBOARD INPUT */
 
          //Stats Event Handler s key
@@ -33,6 +71,7 @@ int main()
 
 
 /* START VIEWER */
+
 
         return (viewer.run());
 }
